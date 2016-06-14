122544ed883d1339845aab4939be3c1b49db4e1f
Hugoo <Hugoo@Hugo>
2016-06-14 11:37:39 +0200

    Phase affichage tableau fini

--- model/src/main/java/model/DAOHelloWorld.java
+++ model/src/main/java/model/DAOHelloWorld.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -11,6 +14,14 @@ import java.sql.SQLException;
  * @author Jean-Aymeric Diet
  */
 class DAOHelloWorld extends DAOEntity<HelloWorld> {
+
+	/** Width */
+	private int height = 1000;
+	
+	/** Height */
+	private int width = 1000;
+	
+	private char[][] tableau = new char[this.getWidth()+1][this.getHeight()+1];
 
 	/**
 	 * Instantiates a new DAO hello world.
@@ -73,17 +84,84 @@ class DAOHelloWorld extends DAOEntity<HelloWorld> {
 			call.execute();
 			final ResultSet resultSet = call.getResultSet();
 			if (resultSet.first()) {
-				helloWorld = new HelloWorld(resultSet.getInt("id"), key, resultSet.getString("message"));
-				Model model = new Model();
-				model.loadFile(resultSet.getString("message"));
-				System.out.println("\n");
-				
+				helloWorld = new HelloWorld(resultSet.getInt("id"), key, "Chargement de la map ...");	
+				String map = resultSet.getString("message");	
+				this.readFile(map);
 			}
+			
 			return helloWorld;
 		} catch (final SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public int getWidth()
+	{
+		return this.width;
+	}
+	
+	public int getHeight()
+	{
+		return this.height;
+	}
+	
+	public void readFile(String map)
+	{		
+		String[] tabmap = map.split("\n"); 
+        for (int i =0; i < tabmap.length; i++)
+        {
+            for (int j = 0; j < tabmap[i].length(); j++)
+            {
+                switch (tabmap[i].charAt(j)){
+                
+                    case 'h':                       
+                    	this.tableau[i][j] = 'h';
+                        System.out.print(this.tableau[i][j]);
+                   break;
+                        
+                    case 'b':                                           
+                    	this.tableau[i][j] = 'b';
+                        System.out.print(this.tableau[i][j]);
+                    break;
+                        
+                    case 'v':                       
+                    	this.tableau[i][j] = 'v';
+                        System.out.print(this.tableau[i][j]);                  
+                    break;
+                    	
+                    case 'p':                       
+                    	this.tableau[i][j] = 'p';
+                        System.out.print(this.tableau[i][j]);
+                    break;
+                        
+                    case 'L':                       
+                        //System.out.print(this.pngArray[i][j]);
+                    break;
+                    
+                    case 'C':                       
+                       // System.out.print(this.pngArray[i][j]);
+                    break;
+                    
+                    case 'K':                       
+                       // System.out.print(this.pngArray[i][j]);
+                    break;
+                    
+                    default:
+                    	this.tableau[i][j] = ' ';
+                        System.out.print(this.tableau[i][j]);
+                    break;
+                }
+            }
+            System.out.println();
+        }
+		
+		/* To show table
+		for(y = 0; y < this.getHeight()+1; y++){
+			for(x = 0; x < this.getWidth()+1; x++){				
+				System.out.print(tableau[x][y]);
+			}
+		}*/
 	}
 
 }
--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -21,14 +21,6 @@ public class Model extends Observable implements IModel {
 
 	/** The message. */
 	private String message;
-
-	/** Width */
-	private int height = 15;
-	
-	/** Height */
-	private int width = 20;
-	
-	private char[][] tableau = new char[this.getWidth()+1][this.getHeight()+1];
 	
 	/**
 	 * Instantiates a new model.
@@ -89,96 +81,5 @@ public class Model extends Observable implements IModel {
 	 */
 	public Observable getObservable() {
 		return this;
-	}
-	
-	public int getWidth()
-	{
-		return this.width;
-	}
-	
-	public int getHeight()
-	{
-		return this.height;
-	}
-	
-	public void loadFile(final String fileName)
-	{
-		try
-		{
-		    File f = new File (fileName);
-		    FileReader fr = new FileReader (f);
-		 
-		    int c = fr.read();
-		    int x = 0;
-		    int y = 0;
-		    
-			while (c != -1)
-			{				
-				if(y < this.getHeight()+1)
-				{	
-					if((char) c != '\r')
-					{
-						this.tableau[x][y] = (char) c;
-						
-						switch((char) c)
-						{						
-							case 'h':
-									
-							break;
-							
-							case 'b':							
-								
-							break;
-							
-							case 'v':
-								
-							break;
-							
-							case 'p':							
-								
-							break;
-							
-							case 'e':							
-								
-							break;
-							
-							case 'c':							
-								
-							break;
-							
-							/*case '':						
-								this.addElement(new Forest(), x, y);	
-								//System.out.print('F');
-							break;*/
-							
-						}
-						
-						x++;
-						
-						if(x == this.getWidth()+1)
-						{
-							y++;
-							x = 0;
-						}
-					}
-				}
-				
-				c = fr.read();
-				
-			}
-			fr.close();
-		
-		}
-		catch (IOException exception)
-		{
-		    System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
-		}
-		
-		// pour afficher le tableau
-		for(int y = 0; y < this.getHeight(); y++){
-			for(int x = 0; x < this.getWidth()+1; x++){
-				System.out.print(tableau[x][y]);
-			}
-		}
 	}
 }
