6b895de719e356f14fbf30e37e17c49442bfa85f
gstal <gstal@10.167.50.195>
2016-06-21 11:23:53 +0200

    Add music behind the game

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -18,6 +18,8 @@ public abstract class Main {
 	 *          the arguments
 	 */
 	public static void main(final String[] args) {
+		Thread playWave=new AePlayWave("C:/Users/gstal/Desktop/Musique.wav");
+        playWave.start();
 		final Model model = new Model();
 		final View view = new View(model);
 		final Controller controller = new Controller(view, model);
